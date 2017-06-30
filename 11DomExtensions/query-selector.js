683bcfc78838b1b17c62f1ff6fc73217ac06baf4
zhangkang <zhangkang@lvmama.com>
2017-06-30 10:12:48 +0800

    querySelector

---
+++ 11DomExtensions/query-selector.js
@@ -0,0 +1,26 @@
+/** css选择器 */
+/*// 按css选择器，返回第一个元素
+var myDiv = document.querySelector(".a");
+console.log(myDiv);
+
+// 返回匹配到的所有元素
+var myDivList = document.querySelectorAll(".a");
+console.log(myDivList);*/
+
+
+// var strongs = document.querySelectorAll("p strong");
+// var i, len;
+// for(i=0, len = strongs.length; i < len; i++) {
+//     strongs[i].className = "important";
+// }
+// console.log(strongs);
+
+
+/** 是否匹配selector，不同浏览器判断方法不同 */
+/*var myDiv = document.getElementById("zk");
+console.log(myDiv.msMatchesSelector); // IE9+
+console.log(myDiv.webkitMatchesSelector); // chrome
+console.log(myDiv.webkitMatchesSelector("#zk"));*/
+
+
+
